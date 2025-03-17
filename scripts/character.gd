dc19cc77c8f977f79af2a548917d40990817e946
DelphinusArcana <zac.girash@gmail.com>
2025-03-17 09:43:24 -0400

    some things

--- scripts/character.gd
+++ scripts/character.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	pass
 
 #The following gotten from here: https://www.reddit.com/r/godot/comments/7xwr22/guide_how_to_click_a_sprite/
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		self.on_click()
 
