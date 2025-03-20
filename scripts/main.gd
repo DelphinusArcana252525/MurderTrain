3c5bb92971cb2cbe77070df756c8126f3d566d2f
Null0Fish <jackson_connors@icloud.com>
2025-03-20 09:00:41 -0400

    added restart

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,3 +7,5 @@ extends Node2D
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_released("ui_accept"):
 		inventory.add_item(str(randi_range(0, 2)))
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
