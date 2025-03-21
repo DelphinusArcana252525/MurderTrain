289d75620a64bc673b4f5099f18d4e37fdaf90d7
Null0Fish <jackson_connors@icloud.com>
2025-03-21 16:54:29 -0400

    added dumb flower pot item and gave items names also edited main

--- scripts/item_info_box.gd
+++ scripts/item_info_box.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
-@onready var label: Label = $Label
-@export var text : String
+@onready var item_name: Label = $ItemName
+@onready var item_description: Label = $ItemDescription
+
+@export var nameVar : String
+@export var descriptionVar : String
 
 func _ready() -> void:
-	label.text = text
+	item_name.text = nameVar
+	item_description.text = descriptionVar
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 @onready var inventory: Node2D = $Camera2D/Inventory
 
-var items : Array = ["bag", "key", "knife", "poster_0", "poster_1"]
+var items : Array = ["bag", "key", "knife", "poster_0", "poster_1", "plant"]
 
 # Temp code for testing
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_released("ui_accept"):
-		inventory.add_item(items[randi_range(0, 4)])
+		inventory.add_item(items[randi_range(0, 5)])
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
